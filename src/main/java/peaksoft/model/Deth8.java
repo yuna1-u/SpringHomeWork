67b20a5ff885335a51f8c058ee0d22441067cfcc
rabia <m1ny00n7177@gmail.com>
2024-05-01 14:07:26 +0300

    first commit

--- src/main/java/peaksoft/model/Deth8.java
+++ src/main/java/peaksoft/model/Deth8.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 public class Deth8 {
     @Override
     public String toString() {
-        return "Deth8{}";
+        return "";
     }
 }
--- src/main/java/peaksoft/model/Needle7.java
+++ src/main/java/peaksoft/model/Needle7.java
@@ -3,7 +3,6 @@ package peaksoft.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Needle7 {
     private Deth8 deth8;
 
